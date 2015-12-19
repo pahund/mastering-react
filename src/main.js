6e97281865a0d69e534c10b2508d740190f4eff5
Patrick Hund <pahund@team.mobile.de>
2015-12-19 16:35:19 +0100

    after completing chapter 1
    Change-Id: I61e784624c38355f87514f36a5d466dbd8d409a2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
+class Heading extends React.Component {
+    render() {
+        return <h1 style={{color: "hotpink" }}>{this.props.children}</h1>
+    }
 }
 
-ReactDOM.render(<App />, document.querySelector("#app"));
+class Greeter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "Fremder"
+        };
+    }
+    render() {
+        return (
+            <div>
+                <Heading>Hallo {this.state.name}!</Heading>
+                <input ref="name" />
+                <button onClick={this.handleGreet.bind(this)}>Greet!!</button>
+            </div>
+        )
+    }
+
+    handleGreet() {
+        this.setState({
+            name: this.refs.name.value
+        });
+    }
+
+    componentWillMount() {
+        console.log("[PH_LOG] component will mount"); // PH_TODO: REMOVE
+    }
+
+    componentDidMount() {
+        console.log("[PH_LOG] component did mount"); // PH_TODO: REMOVE
+    }
+
+    componentWillReceiveProps() {
+        console.log("[PH_LOG] component will receive props"); // PH_TODO: REMOVE
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        const shouldUpdate = nextState.name !== this.state.name;
+        console.log("[PH_LOG] should component update?", shouldUpdate); // PH_TODO: REMOVE
+        return shouldUpdate;
+    }
+
+    componentWillUpdate() {
+        console.log("[PH_LOG] component will update"); // PH_TODO: REMOVE
+    }
+
+    componentDidUpdate() {
+        console.log("[PH_LOG] component did update"); // PH_TODO: REMOVE
+    }
+
+    componentWillUnount() {
+        console.log("[PH_LOG] component will unmount"); // PH_TODO: REMOVE
+    }
+
+
+
+}
+
+const GreeterFactory = React.createFactory(Greeter);
+class App extends React.Component {
+    render() {
+        return <Greeter name="WORRRRLD" />
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector("#app"));
